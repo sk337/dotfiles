2d80d6e8412cbc6b1969462bcfe54f593a2f133a
sk337 <79583517+sk337@users.noreply.github.com>
2025-04-18 11:36:03 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     lsd
+
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+zoxide init bash --cmd cd >> ~/.bashrc
+
+
+
 
